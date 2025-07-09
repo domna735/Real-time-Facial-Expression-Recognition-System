2e44159a3687b44b911848791645aa7e2913b650
domna735 <domna735@gmail.com>
2025-07-09 11:28:08 +0800

    Fix env_test to use matplotlib.__version__

---
+++ src/env_test.py
@@ -0,0 +1,11 @@
+# src/env_test.py
+
+import torch
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("PyTorch:", torch.__version__)
+print("OpenCV:", cv2.__version__)
+print("NumPy:", np.__version__)
+print("Matplotlib:", plt.__version__)
